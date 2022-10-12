e8b913ff0af6c5e155994cc1f6ed99ae1da3ef54
Gal Wender <galwender@gmail.com>
2022-10-12 16:43:05 +0300

    final working product

--- src/pages/test.jsx
+++
@@ -1,5 +0,0 @@
-export const Test = () => {
-  return (
-    <h1>Hello TESTING</h1>
-  )
-}
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,11 @@
 import { Cart } from './pages/cart.jsx'
 import { Home } from './pages/home.jsx'
-import { Test } from './pages/test.jsx'
 
 const routes = [
     {
         path: '/',
         component: <Home />,
         label: 'Home',
-    },
-    {
-        path: '/test',
-        component: <Test />,
     },
     {
         path: '/cart',
