3c90c3f26a1eb0fba412798e67ea4c60633c4919
TalBenAtiya <benatia.tal@gmail.com>
2022-10-12 12:55:20 +0200

    changed favicon and init project

--- src/services/name.service.js
+++ src/services/name.service.js
@@ -1,61 +1,3 @@
-//for http
-
-
-// import { storageService } from './async-storage.service.js'
-// import { utilService } from './util.service.js'
-// import { userService } from './user.service.js'
-// import { httpService } from './http.service.js'
-// import { socketService, SOCKET_EMIT_SEND_MSG, SOCKET_EVENT_ADD_MSG } from './socket.service.js'
-// import { getActionAddMsg } from '../store/actions/toy.action.js'
-// import {store} from '../store/store'
-
-// // for local storage
-// const STORAGE_KEY = 'nameDB'
-
-// export const toyService = {
-//     query,
-//     getById,
-//     save,
-//     remove,
-// }
-
-// const BASE_URL = `name/`
-
-// /* ?- WebSocket */;
-// (() => {
-//     socketService.on(SOCKET_EMIT_SEND_MSG, (msg) => {
-//       console.log('GOT msg from socket', msg)
-//       store.dispatch(getActionAddMsg(msg))
-//     })
-//     socketService.on(SOCKET_EVENT_ADD_MSG, (msg) => {
-//       console.log('GOT msg from socket', msg)
-//       store.dispatch(getActionAddMsg(msg))
-//     })
-//   })()
-
-// function query(filterBy) {
-//     console.log(filterBy)
-//     return httpService.get(BASE_URL, { params: filterBy })
-// }
-// function getById(toyId) {
-//     return httpService.get(BASE_URL + toyId)
-// }
-// function remove(toyId) {
-//     // return Promise.reject('Not now!');
-//     return httpService.delete(BASE_URL + toyId)
-// }
-// function save(toy) {
-//     if (toy._id) {
-//         return httpService.put(BASE_URL + toy._id, toy)
-//     } else {
-//         // When switching to backend - remove the next line!
-//         return httpService.post(BASE_URL, toy)
-//     }
-// }
-
-
-// for local
-
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
@@ -74,9 +16,7 @@ function query(filterBy) {
     return storageService.query(STORAGE_KEY)
         .then((toys) => {
             // toys = (filterBy.category === 'ACTIVE') ?
-            //     todos.filter(todo => !todo.isDone) :
             //     (filterBy.category === 'DONE') ?
-            //         todos.filter(todo => todo.isDone) : todos
             // if (filterBy.term && filterBy.term !== '') todos = todos.filter(todo => todo.txt.toLowerCase().includes(filterBy.term.toLowerCase()))
             return toys
         })
--- src/store/actions/name.action.js
+++ src/store/actions/name.action.js
@@ -5,16 +5,6 @@
 // import { taskService } from "../../services/task.service"
 
 
-export function getActionUpdatename(name) {
-    return { type: 'UPDATE_name', name }
-}
-
-export function setLoader() {
-    return (dispatch, getState) => {
-        dispatch({ type: 'SET_LOADING', isLoading: true })
-    }
-}
-
 export function loadnames() {
     return async (dispatch, getState) => {
         try {
@@ -43,18 +33,6 @@ export function removename(nameId) {
     }
 }
 
-export function setFilter(filterBy) {
-    return (dispatch) => {
-        dispatch({ type: 'SET_FILTER', filterBy })
-    }
-}
-
-export function setSort(sortBy) {
-    return (dispatch) => {
-        dispatch({ type: 'SET_SORT', sortBy })
-    }
-}
-
 export function loadname(nameId, sortBy, filterBy) {
     return async (dispatch, getState) => {
         try {
@@ -67,32 +45,3 @@ export function loadname(nameId, sortBy, filterBy) {
         }
     }
 }
-
-export function addname(name) {
-    return async (dispatch, getState) => {
-        try {
-            dispatch({ type: 'SET_LOADING', isLoading: true })
-            const savedname = await nameService.save(name)
-            dispatch({ type: 'ADD_name', name: savedname })
-        } catch (err) {
-            // showErrorMsg('Failed to add name try again')
-        } finally {
-            dispatch({ type: 'SET_LOADING', isLoading: false })
-        }
-    }
-}
-
-export function updatename(name) {
-    return async (dispatch, getState) => {
-        try {
-            await nameService.save(name)
-            // const updatedname = await nameService.save(name)
-            // dispatch({ type: 'UPDATE_name', name: updatedname })
-        } catch (err) {
-            // showErrorMsg('Failed to update name')
-        } finally {
-            dispatch({ type: 'SET_LOADING', isLoading: false })
-        }
-    }
-}
-
