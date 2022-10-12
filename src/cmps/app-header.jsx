cdcbe3e7a23e716bcc77be7ca41b4af566420fbc
Gal Wender <galwender@gmail.com>
2022-10-12 15:01:41 +0300

    ongoing app header

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,4 +1,16 @@
+import { ReactComponent as HouseSvg } from '../assets/svg/house-icon.svg'
+import { ReactComponent as CartSvg } from '../assets/svg/cart-icon.svg'
+import { NavLink } from "react-router-dom";
 export const AppHeader = () => {
     return <nav className="app-header">
+        <div className='main-layout header-container'>
+            <NavLink to='/' className="btn btn-svg-house"><HouseSvg /></NavLink>
+            <button className="btn btn-checkout">
+                <CartSvg className="btn btn-svg-cart" />
+                <p>0</p>
+                <p>ITEMS</p>
+                <p>$2</p>
+            </button>
+        </div>
     </nav>
 }
