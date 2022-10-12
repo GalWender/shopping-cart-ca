96f5acc7b49ebb18d88d0e9065e0e6b162cf60a0
TalBenAtiya <benatia.tal@gmail.com>
2022-10-12 13:32:43 +0200

    home init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 
 import routes from './routes'
-
+import { Home } from './pages/home'
 import { AppHeader } from './cmps/app-header'
 import { AppFooter } from './cmps/app-footer'
 import { Test } from './pages/test'
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,9 @@
+import homeImg from '../assets/img/home-img.jpg'
 
 export const Home = () => {
 
     return <section className="home">
+        <div className="home-hero"><img src={homeImg} /></div>
         
     </section>
 }
